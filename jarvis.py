fbe27890e12e975f9745eefc076ff20b792ec370
Rishu Kumari <rishusingh3638@gmail.com>
2025-04-26 13:37:20 +0530

    Add files via upload

---
+++ jarvis.py
@@ -0,0 +1,67 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
+import webbrowser
+import os 
+import pyaudio   
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak("Good Morning!")
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon!")
+    else:
+        speak("Good Evening!")
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language = 'en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print("Say that again please...")
+        return "None"
+    return query
+
+
+if __name__=="__main__":
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+        if 'wikipedia' in query:
+            speak('Searching wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences = 1)
+            speak("According to wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")  
+        elif 'open instagram' in query:
+             webbrowser.open("instagram.com")
+        elif 'open snapchat' in query:
+             webbrowser.open("snapchat.com")     
+        elif 'open spotify' in query:
+            webbrowser.open("spotiffy.com")
+            
+            
